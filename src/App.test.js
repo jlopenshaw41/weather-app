c1022878d620f264bbaad4c54edb70d892a697ea
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2021-02-03 10:30:25 +0000

    Updated test to check that "Weather App" header renders successfully

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders "Weather App" header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const weatherAppHeader = screen.getByText('Weather App');
+  expect(weatherAppHeader).toBeInTheDocument();
 });
