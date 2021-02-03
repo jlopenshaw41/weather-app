b8f67fce3689bcfe711c103a2faadf10eaa3317a
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2021-02-03 17:08:04 +0000

    Resolved issue with prop-types by installing as dependency (as per error message) and hitting save in the relevant files. Completed track to get to point where city and country display in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "../styles/App.css";
+import PropTypes from "prop-types";
+import LocationDetails from "./LocationDetails";
 
-function App() {
+function App(props) {
+  const { location } = props;
   return (
-    <div className="prop">
-      <h1>Weather App</h1>
+    <div className="App">
+      <LocationDetails city={location.city} country={location.country} />
     </div>
   );
 }
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
 
 export default App;
