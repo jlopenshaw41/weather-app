af94b02658fd6a2ab54346273a24d7ecf60605c7
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2021-02-09 11:41:04 +0000

    Added "More details" button to ForecastSummary component and added event handler to display correct Forecast Details when clicked

--- src/components/App.js
+++ src/components/App.js
@@ -12,10 +12,15 @@ const App = ({ forecasts, location }) => {
     (forecast) => forecast.date === selectedDate
   );
 
+  const handleForecastSelect = (date) => setSelectedDate(date);
+
   return (
     <div className="weather-app">
       <LocationDetails city={location.city} country={location.country} />
-      <ForecastSummaries forecasts={forecasts} />
+      <ForecastSummaries
+        forecasts={forecasts}
+        onForecastSelect={handleForecastSelect}
+      />
       <ForecastDetails forecast={selectedForecast} />
     </div>
   );
