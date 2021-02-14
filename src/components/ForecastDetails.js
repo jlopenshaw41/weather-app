bc63ddf6dc705d9be265efc44437fe2ccc6d346f
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2021-02-14 11:48:44 +0000

    Styled forecast details section

--- src/components/ForecastDetails.js
+++ src/components/ForecastDetails.js
@@ -1,38 +1,47 @@
 import React from "react";
 import PropTypes from "prop-types";
+import WeatherIcon from "react-icons-weather";
 import moment from "moment";
 import "../styles/ForecastDetails.css";
 
 const ForecastDetails = ({ forecast }) => {
-  const { date, temperature, wind, humidity } = forecast;
+  const { date, temperature, wind, humidity, icon, description } = forecast;
   return (
     <div className="forecast-details" data-testid="forecast-details">
       <div className="forecast-details__date">
         {moment(date).format("ddd Do MMM")}
       </div>
-      <div className="forecast-details__min-temperature">
-        Min temp:
-        {temperature.min}
-        &deg;C
-      </div>
-      <div className="forecast-details__max-temperature">
-        Max temp:
-        {temperature.max}
-        &deg;C
-      </div>
-      <div className="forecast-details__humidity">
-        Humidity:
-        {humidity}
-        &#37;
-      </div>
-      <div className="forecast-details__wind-speed">
-        Wind speed:
-        {wind.speed}
-        mph
-      </div>
-      <div className="forecast-details__wind-direction">
-        Wind direction:
-        {wind.direction}
+      <div className="forecast-details__body">
+        <div className="forecast-details__icon">
+          <WeatherIcon name="owm" iconId={icon} />
+        </div>
+        <div className="forecast-details__body__text">
+          <div className="forecast-details__description">{description}</div>
+          <div className="forecast-details__min-temperature">
+            Min temp:
+            {` ${temperature.min}`}
+            &deg;C
+          </div>
+          <div className="forecast-details__max-temperature">
+            Max temp:
+            {` ${temperature.max}`}
+            &deg;C
+          </div>
+          <div className="forecast-details__humidity">
+            Humidity:
+            {` ${humidity}`}
+            &#37;
+          </div>
+          <div className="forecast-details__wind-speed">
+            Wind speed:
+            {` ${wind.speed}`}
+            mph
+          </div>
+          <div className="forecast-details__wind-direction">
+            Wind direction:
+            {` ${wind.direction.toUpperCase()}`}
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -42,8 +51,10 @@ export default ForecastDetails;
 
 ForecastDetails.propTypes = {
   forecast: PropTypes.shape({
+    description: PropTypes.string.isRequired,
     date: PropTypes.number.isRequired,
     humidity: PropTypes.number.isRequired,
+    icon: PropTypes.string.isRequired,
     temperature: PropTypes.shape({
       max: PropTypes.number,
       min: PropTypes.number,
