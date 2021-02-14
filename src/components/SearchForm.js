040df7a485ee4cb62a2ccf9b6886791f4d6c020f
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2021-02-14 11:15:22 +0000

    Added font-awesome for search icon. Added event handler to make search work when hitting enter. Styled button

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,15 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "../styles/SearchForm.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const SearchForm = ({ searchText, setSearchText, onSubmit }) => {
   const handleInputChange = (event) => setSearchText(event.target.value);
 
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      onSubmit();
+    }
+  };
+
   return (
     <div className="search-form">
-      <input type="text" onChange={handleInputChange} value={searchText} />
+      <input
+        type="text"
+        onChange={handleInputChange}
+        value={searchText}
+        onKeyUp={handleKeyPress}
+      />
       <button type="submit" onClick={onSubmit}>
-        Search
+        <FontAwesomeIcon className="search-icon" icon={faSearch} />
       </button>
     </div>
   );
