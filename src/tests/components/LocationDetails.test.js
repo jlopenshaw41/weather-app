eb07e48dc0e6d570c4df7840cab00bcd9e9aaf2a
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2021-02-16 18:30:53 +0000

    Made app mobile-responsive

--- src/tests/components/LocationDetails.test.js
+++ src/tests/components/LocationDetails.test.js
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react";
 import LocationDetails from "../../components/LocationDetails";
 
 describe("LocationDetails", () => {
-  it("renders the correct city and country props", () => {
+  it("renders the correct city prop", () => {
     const { getByText } = render(<LocationDetails city="Manchester" />);
     expect(getByText("Manchester")).toHaveClass("location-details");
   });
